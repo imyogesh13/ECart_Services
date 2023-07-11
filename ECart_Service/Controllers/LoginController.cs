a328e0cd2026b8364c1a998c73f15c9c4455c2bc
imyogesh13 <patil.yps@gmail.com>
2023-07-10 21:52:15 -0700

    Login controller added

---
+++ ECart_Service/Controllers/LoginController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ECart_Service.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
